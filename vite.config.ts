46f888a04fcd0f7a98071cad3e58a25e67df4a2b
RaúlCollazo <195011609+RaulCollazo@users.noreply.github.com>
2026-09-13 20:09:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Portafolio/",
   server: {
-    host: "0.0.0.0", // ← Importante
+    host: "0.0.0.0",
     port: 3000,
   },
 });
